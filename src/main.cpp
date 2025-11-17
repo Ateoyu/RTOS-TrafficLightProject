9ce391f8d6d3881a18321542f45f761bd66fb594
Ateoyu <ateosu@proton.me>
2025-11-17 20:25:31 +0100

    Add Button ISR
    Buttons ISR works.
    However, it doesnt reset the 10 second counter in default task, so it can lead to situations where the lights switch green and then back red really quickly on ISR.
    Need to implement better logic to reset the 10 second timer after ISR event.

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,9 @@
 #include <queue.h>
 #include <cstdio>
 
-QueueHandle_t xQueue;
-SemaphoreHandle_t semaphoreSensor;
-SemaphoreHandle_t semaphoreButton;
+QueueHandle_t xLoggerQueue;
+SemaphoreHandle_t sensorSemaphore;
+SemaphoreHandle_t buttonSemaphore;
 
 const int trafficLightRed = 6;
 const int trafficLightYellow = 5;
@@ -22,11 +22,6 @@ const int rangeFinderTrig = 18;
 const float soundSpeed = 0.034;
 
 const int button = 10;
-int currentButtonState;
-int lastButtonState = HIGH;
-
-volatile bool pedestrianCrossingOn = false;
-
 //queue struct
 enum Cause { NORMAL, BUTTON, SENSOR };
 
@@ -35,131 +30,87 @@ typedef struct {
 } LightCommand;
 
 //Light transitions
-void redToGreen(const int redLedPin, const int amberLedPin, const int greenLedPin) {
+void redToGreenTraffic(const int redLedPin, const int yellowLedPin, const int greenLedPin) {
     digitalWrite(redLedPin, HIGH);
-    digitalWrite(amberLedPin, HIGH);
+    digitalWrite(yellowLedPin, HIGH);
     vTaskDelay(pdMS_TO_TICKS(600));
     digitalWrite(redLedPin, LOW);
-    digitalWrite(amberLedPin, LOW);
+    digitalWrite(yellowLedPin, LOW);
     digitalWrite(greenLedPin, HIGH);
 }
 
-void greenToRed(const int redLedPin, const int amberLedPin, const int greenLedPin) {
+void greenToRedTraffic(const int redLedPin, const int yellowLedPin, const int greenLedPin) {
     digitalWrite(greenLedPin, HIGH);
     vTaskDelay(pdMS_TO_TICKS(600));
     digitalWrite(greenLedPin, LOW);
-    digitalWrite(amberLedPin, HIGH);
+    digitalWrite(yellowLedPin, HIGH);
     vTaskDelay(pdMS_TO_TICKS(600));
-    digitalWrite(amberLedPin, LOW);
+    digitalWrite(yellowLedPin, LOW);
     digitalWrite(redLedPin, HIGH);
 }
 
-// ISR Handling
-void IRAM_ATTR pedestrianButton() {
-    currentButtonState = digitalRead(button);
+void redToGreenPedestrian(const int redLed, const int greenLed) {
+    digitalWrite(redLed, LOW);
+    digitalWrite(greenLed, HIGH);
+}
 
-    if (!pedestrianCrossingOn && lastButtonState == LOW && currentButtonState == HIGH) {
-        BaseType_t xHigherPriorityTaskWoken = pdFALSE;
-        xSemaphoreGiveFromISR(semaphoreButton, &xHigherPriorityTaskWoken);
-        portYIELD_FROM_ISR(xHigherPriorityTaskWoken);
+void greenToRedPedestrian(const int redLed, const int greenLed) {
+    digitalWrite(redLed, HIGH);
+    digitalWrite(greenLed, LOW);
+}
+
+void triggerLightSwitch() {
+    if (digitalRead(pedestrianLightGreen) == HIGH) {
+        redToGreenTraffic(trafficLightRed, trafficLightYellow, trafficLightGreen);
+        greenToRedPedestrian(pedestrianLightRed, pedestrianLightGreen);
+    } else {
+        greenToRedTraffic(trafficLightRed, trafficLightYellow, trafficLightGreen);
+        redToGreenPedestrian(pedestrianLightRed, pedestrianLightGreen);
     }
-    lastButtonState = currentButtonState;
 }
 
-void vehicleSensor() { //TODO:
-    // Clears the rangeFinderTrig
-    digitalWrite(rangeFinderTrig, LOW);
-    delayMicroseconds(2);
-    // Sets the rangeFinderTrig on HIGH state for 10 micro seconds
-    digitalWrite(rangeFinderTrig, HIGH);
-    delayMicroseconds(10);
-    digitalWrite(rangeFinderTrig, LOW);
-
-    // Reads the rangeFinderEcho, returns the sound wave travel time in microseconds
-    unsigned long duration = pulseIn(rangeFinderEcho, HIGH);
-
-    // Calculate the distance
-    float distanceCm = duration * soundSpeed / 2;
-
-    // Prints the distance in the Serial Monitor
-    Serial.print("Distance (cm): ");
-    Serial.println(distanceCm);
+void IRAM_ATTR handlePedestrianButtonISR() {
+    BaseType_t xHigherPriorityTaskWoken = pdFALSE;
+    xSemaphoreGiveFromISR(buttonSemaphore, &xHigherPriorityTaskWoken);
+    portYIELD_FROM_ISR(xHigherPriorityTaskWoken);
 }
 
-// every 10s change from pedestrian -> car (add tasks to queue)
-// if pedestrianButton sends ISR, change after 1s instead of 10s (execute next switch from queue)
-// if vehicleSensor sends ISR, change after 3s instead of 10s (execute next switch from queue)
-// if pedestrianCrossing on == buzzer buzz
-// log all events into Serial via taskLogger
-
-// queue works on bool,
-// true -> green light for car, red for pedestrian
-// false -> red light for car, green for pedestrian
-// differentiate how through struct entry, labelling what caused the change to decide how long to wait till change.
-
-//TASK 1 (change lights)
-void changeLights(void *pvParameters) {
-    LightCommand cmd;
-
+[[noreturn]] void taskChangeLightsFromPedestrianButtonISR(void *pv) {
     for (;;) {
-        if (xQueueReceive(xQueue, &cmd, portMAX_DELAY) == pdPASS) {
-            pedestrianCrossingOn = !pedestrianCrossingOn;
-
-            if (pedestrianCrossingOn) {
-                // Pedestrians green
-                greenToRed(trafficLightRed, trafficLightYellow, trafficLightGreen);
-                digitalWrite(pedestrianLightRed, LOW);
-                digitalWrite(pedestrianLightGreen, HIGH);
+        if (xSemaphoreTake(buttonSemaphore, portMAX_DELAY) == pdTRUE) {
+            if (digitalRead(trafficLightGreen) == HIGH) {
+                vTaskDelay(pdMS_TO_TICKS(300));
+                triggerLightSwitch();
+                LightCommand cmd = {BUTTON};
+                xQueueSend(xLoggerQueue, &cmd, 0);
             } else {
-                // Cars green
-                digitalWrite(pedestrianLightGreen, LOW);
-                digitalWrite(pedestrianLightRed, HIGH);
-                redToGreen(trafficLightRed, trafficLightYellow, trafficLightGreen);
+                Serial.printf("Button pressed, but pedestrian already have green light.\n");
             }
         }
     }
 }
 
-//TASK 2 (buzzer)
-void buzzerSound(void *pvParameters) {
+[[noreturn]] void taskDefaultLightBehaviour(void *pv) {
     for (;;) {
-        if (pedestrianCrossingOn) {
-            tone(buzzer, NOTE_C4, 100);
-            vTaskDelay(pdMS_TO_TICKS(150));
-        }
-        vTaskDelay(pdMS_TO_TICKS(100));
+        vTaskDelay(pdMS_TO_TICKS(10000));
+        triggerLightSwitch();
+        LightCommand cmd = {NORMAL};
+        xQueueSend(xLoggerQueue, &cmd, 0);
     }
 }
 
-//TASK 3 (main controller)
-void taskController(void *pvParameters) {
+[[noreturn]] void taskLogger(void *pv) {
+    LightCommand receivedCmd;
     for (;;) {
-        if (xSemaphoreTake(semaphoreButton, portMAX_DELAY) == pdTRUE) {
-            LightCommand cmd = {BUTTON};
-            xQueueSend(xQueue, &cmd, 0);
-        } else if (xSemaphoreTake(semaphoreSensor, portMAX_DELAY) == pdTRUE) {
-            LightCommand cmd = {SENSOR};
-            xQueueSend(xQueue, &cmd, 0);
-        } else {
-            vTaskDelay(pdMS_TO_TICKS(10000));
-            LightCommand cmd = {NORMAL};
-            xQueueSend(xQueue, &cmd, 0);
+        if (xQueueReceive(xLoggerQueue, &receivedCmd, portMAX_DELAY) == pdPASS) {
+            switch (receivedCmd.cause) {
+                case NORMAL: Serial.printf("Received Normal light switch command.\n"); break;
+                case BUTTON: Serial.printf("Received Button light switch command.\n"); break;
+                case SENSOR: Serial.printf("Received Sensor light switch command.\n"); break;
+            }
         }
     }
 }
-
-//TASK 4 (logger)
-void taskLogger(void *pvParameters) {
-    LightCommand log;
-
-    for (;;) {
-        if (xQueueReceive(xQueue, &log, portMAX_DELAY) == pdPASS) {
-            Serial.printf("LOG → Cause: %d, pedestrianCrossingOn: %d\n",
-                          log.cause, pedestrianCrossingOn);
-        }
-    }
-}
-
 
 void setup() {
     Serial.begin(115200);
@@ -174,21 +125,17 @@ void setup() {
     pinMode(rangeFinderTrig, OUTPUT);
     pinMode(button, INPUT_PULLUP);
 
-    xQueue = xQueueCreate(5, sizeof(LightCommand));
-    semaphoreSensor = xSemaphoreCreateBinary();
-    semaphoreButton = xSemaphoreCreateBinary();
+    digitalWrite(trafficLightGreen, HIGH);
+    digitalWrite(pedestrianLightRed, HIGH);
 
-    if (xQueue != nullptr && semaphoreSensor != nullptr && semaphoreButton != nullptr) {
-        attachInterrupt(digitalPinToInterrupt(button), pedestrianButton, CHANGE);
+    sensorSemaphore = xSemaphoreCreateBinary();
+    buttonSemaphore = xSemaphoreCreateBinary();
+    xLoggerQueue = xQueueCreate(5, sizeof(LightCommand));
+    attachInterrupt(digitalPinToInterrupt(button), handlePedestrianButtonISR, FALLING);
 
-        xTaskCreate(changeLights, "changeLights", 2000, nullptr, 1, nullptr);
-        xTaskCreate(buzzerSound, "buzzer", 1000, nullptr, 1, nullptr);
-        xTaskCreate(taskController, "controller", 2000, nullptr, 2, nullptr);
-        xTaskCreate(taskLogger, "logger", 2000, nullptr, 1, nullptr);
-    } else {
-        printf("Failed to create queue\n");
-    }
+    xTaskCreate(taskDefaultLightBehaviour, "default", 2048, nullptr, 1, nullptr);
+    xTaskCreate(taskLogger, "logger", 2048, nullptr, 1, nullptr);
+    xTaskCreate(taskChangeLightsFromPedestrianButtonISR, "changeLightsButtonISR", 2048, nullptr, 2, nullptr);
 }
 
-void loop() {
-}
+void loop() {}
